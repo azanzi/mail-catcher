7c09d9532a1e5894101fee12ab6a5aff5be38d20
Alessandro Zanzi <alessandro.zanzi@usi.ch>
2023-09-13 19:19:37 +0200

    implemented showing of email data in template

--- handlers.go
+++ handlers.go
@@ -2,8 +2,14 @@ package main
 
 import "net/http"
 
+// landingPageHandler shows the list of all emails in the inbox.
 func (app *application) landingPageHandler(w http.ResponseWriter, r *http.Request) {
-
+	data := struct {
+		Emails []*Email
+	}{
+		Emails: app.inbox,
+	}
+	app.templates["emails"].Execute(w, data)
 }
 
 func (app *application) emailPageHandler(w http.ResponseWriter, r *http.Request) {
