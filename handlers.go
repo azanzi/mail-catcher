57abc0a96f61f19ac86a8e5d1197ed6dc700a11c
Alessandro Zanzi <alessandro.zanzi@usi.ch>
2023-09-13 19:41:51 +0200

    added display of single email

--- handlers.go
+++ handlers.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"strconv"
+)
 
 // landingPageHandler shows the list of all emails in the inbox.
 func (app *application) landingPageHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,6 +16,20 @@ func (app *application) landingPageHandler(w http.ResponseWriter, r *http.Reques
 	app.templates["emails"].Execute(w, data)
 }
 
+// emailPageHandler shows the email at position 'id'.
 func (app *application) emailPageHandler(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil || id < 0 || id >= len(app.inbox) {
+		// TODO: out of range, handle error
+	}
 
+	data := struct {
+		Emails []*Email
+		Email  *Email
+	}{
+		Emails: app.inbox,
+		Email:  app.inbox[id],
+	}
+	app.templates["emails"].Execute(w, data)
 }
